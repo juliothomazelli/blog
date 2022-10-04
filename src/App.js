3188764b99cceb9840aa5f78923d9fce5712a063
juliothomazelli <juliothomazelli@gmail.com>
2022-10-04 00:42:04 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -168,7 +168,7 @@ export function App() {
                         Julio Thomazelli
                     </span>
                 </div>
-                <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:white hover:underline">
                   Leia mais
                   <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </a>
@@ -192,7 +192,7 @@ export function App() {
                       Julio Thomazelli
                   </span>
                 </div>
-                <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-white hover:underline">
                   Leia mais
                   <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </a>

6beeb9eaeef8bb8344b098a86d8ba8882c60b551
juliothomazelli <juliothomazelli@gmail.com>
2022-10-09 23:04:23 -0300

    changes

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white">
       <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
-        <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
-          <span className="text-teal-400">Free Register</span> to receive news
+        <h1 className="text-2xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
+          <span className="text-teal-400">Cadastre-se</span> para receber as novas postagem
         </h1>
         <div>
           <input type="text" placeholder="Enter your email" className="text-gray-800 sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"/>

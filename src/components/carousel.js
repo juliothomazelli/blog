72f6aaf60e8e5d68bdbdd9bc6643bbc2e59f6f3d
juliothomazelli <juliothomazelli@gmail.com>
2022-11-01 23:58:12 -0300

    changes

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,176 +1,26 @@
-import React, { useState, useEffect, } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
-const Photo02_full      = require("../assets/photo02-full.jpeg");
-const Photo03           = require("../assets/photo03.jpeg");
-const Photo04           = require("../assets/photo04.jpeg");
-const Photo05           = require("../assets/photo05.jpeg");
-const Photo06           = require("../assets/photo06.jpeg");
-const Photo06_full      = require("../assets/photo06-full.jpeg");
-const Photo06_full_mini = require("../assets/photo06-full-mini.jpeg");
-const Photo07           = require("../assets/photo07.jpeg");
-const Photo08           = require("../assets/photo08.jpeg");
-const Photo09           = require("../assets/photo09.jpeg");
-const Photo10           = require("../assets/photo10.jpeg");
-const Photo11           = require("../assets/photo11.jpeg");
-const Photo12           = require("../assets/photo12.jpeg");
+const Photo10      = require("../assets/photo10.jpeg");
+const Photo11      = require("../assets/photo11.jpeg");
+const Photo12      = require("../assets/photo12.jpeg");
 
-export default function Carousel(props) {
-  const [image, setImage] = useState(props.image);
-  
-  function getImageHTML(image){
-    let html;
-
-    if (image == 1){
-      html =  (
-        <div className="carousel-item relative w-full">
-          <img src={Photo02_full} className=""/>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <div onClick={() => { setImage(9) }} className="btn btn-circle bg-red">❮</div>
-            <div onClick={() => { setImage(2) }} className="btn btn-circle">❯</div>
-          </div>
-        </div>
-      );
-    }
-
-    if (image == 2){
-      html = (
-        <div className="carousel-item relative w-full">
-          <img src={Photo03}/>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a onClick={() => { setImage(1) }} className="btn btn-circle">❮</a> 
-            <a onClick={() => { setImage(3) }} className="btn btn-circle">❯</a>
-          </div>
-        </div>
-      );
-    }
-
-    if (image == 3){
-      html = (
-        <div className="carousel-item relative w-full">
-          <img src={Photo04}/>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a onClick={() => { setImage(2) }} className="btn btn-circle">❮</a> 
-            <a onClick={() => { setImage(4) }} className="btn btn-circle">❯</a>
-          </div>
-        </div>
-      );
-    }
-
-    if (image == 4){
-      html = (
-        <div className="carousel-item relative w-full">
-          <img src={Photo05}/>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a onClick={() => { setImage(3) }} className="btn btn-circle">❮</a> 
-            <a onClick={() => { setImage(5) }} className="btn btn-circle">❯</a>
-          </div>
-        </div>
-      );
-    }
-
-    if (image == 5){
-      html = (
-        <div className="carousel-item relative w-full flex justify-center">
-          <img src={Photo06_full_mini}/>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a onClick={() => { setImage(4) }} className="btn btn-circle">❮</a> 
-            <a onClick={() => { setImage(6) }} className="btn btn-circle">❯</a>
-          </div>
-        </div>
-      );
-    }
-
-    if (image == 6){
-      html = (
-        <div className="carousel-item relative w-full">
-          <img src={Photo11} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a onClick={() => { setImage(5) }} className="btn btn-circle">❮</a> 
-            <a onClick={() => { setImage(7) }} className="btn btn-circle">❯</a>
-          </div>
-        </div>
-      );
-    }
-
-    if (image == 7){
-      html = (
-        <div className="carousel-item relative w-full">
-          <img src={Photo07} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a onClick={() => { setImage(6) }} className="btn btn-circle">❮</a> 
-            <a onClick={() => { setImage(8) }} className="btn btn-circle">❯</a>
-          </div>
-        </div>
-      );
-    }
-
-    if (image == 8){
-      html = (
-        <div className="carousel-item relative w-full">
-          <img src={Photo09} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a onClick={() => { setImage(7) }} className="btn btn-circle">❮</a> 
-            <a onClick={() => { setImage(9) }} className="btn btn-circle">❯</a>
-          </div>
-        </div>
-      );
-    }
-
-    if (image == 9){
-      html = (
-        <div className="carousel-item relative w-full">
-          <img src={Photo08} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a onClick={() => { setImage(8) }} className="btn btn-circle">❮</a> 
-            <a onClick={() => { setImage(1) }} className="btn btn-circle">❯</a>
-          </div>
-        </div>
-      );
-    }
-
-    return html;
-  }
-
+export default function Carousel() {
   return (
-    <div>
-      <div className="carousel w-full h-full">
-        {
-          image == 1 ? getImageHTML(1) : undefined
-        }
-
-        {
-          image == 2 ? getImageHTML(2) : undefined
-        }
-
-        {
-          image == 3 ? getImageHTML(3) : undefined
-        }
-        
-        {
-          image == 4 ? getImageHTML(4) : undefined
-        }
-
-        {
-          image == 5 ? getImageHTML(5) : undefined
-        }
-
-        {
-          image == 6 ? getImageHTML(6) : undefined
-        }
-
-        {
-          image == 7 ? getImageHTML(7) : undefined
-        }
-
-        {
-          image == 8 ? getImageHTML(8) : undefined
-        }
-
-        {
-          image == 9 ? getImageHTML(9) : undefined
-        }
-
-      </div>
-    </div>
+    <>
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+        <SwiperSlide>
+          <img className="object-fill w-full h-96" src="https://cdn.pixabay.com/photo/2022/03/20/15/40/nature-7081138__340.jpg" alt="image slide 1"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="object-fill w-full h-96" src="https://cdn.pixabay.com/photo/2022/07/24/17/55/wind-energy-7342177__340.jpg" alt="image slide 2"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="object-fill w-full h-96" src="https://cdn.pixabay.com/photo/2022/07/26/03/35/jogger-7344979__340.jpg" alt="image slide 3"/>
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 }
--- src/pages/photo.js
+++ src/pages/photo.js
@@ -1,99 +1,80 @@
-import React, { useState, useEffect, } from 'react';
-
-import Carousel from "../components/carousel";
-import Modal from "../components/modal"
+import * as React from "react";
 
 const Photo02_full = require("../assets/photo02-full.jpeg");
-const Photo03 = require("../assets/photo03.jpeg");
-const Photo04 = require("../assets/photo04.jpeg");
-const Photo05 = require("../assets/photo05.jpeg");
-const Photo06 = require("../assets/photo06.jpeg");
-const Photo07 = require("../assets/photo07.jpeg");
-const Photo08 = require("../assets/photo08.jpeg");
-const Photo09 = require("../assets/photo09.jpeg");
-const Photo10 = require("../assets/photo10.jpeg");
-const Photo11 = require("../assets/photo11.jpeg");
-const Photo12 = require("../assets/photo12.jpeg");
+const Photo03      = require("../assets/photo03.jpeg");
+const Photo04      = require("../assets/photo04.jpeg");
+const Photo05      = require("../assets/photo05.jpeg");
+const Photo06      = require("../assets/photo06.jpeg");
+const Photo07      = require("../assets/photo07.jpeg");
+const Photo08      = require("../assets/photo08.jpeg");
+const Photo09      = require("../assets/photo09.jpeg");
+const Photo10      = require("../assets/photo10.jpeg");
+const Photo11      = require("../assets/photo11.jpeg");
+const Photo12      = require("../assets/photo12.jpeg");
 
 export default function Photo() {
   return (
     <div>
-      <h1 className="text-center font-bold py-7 text-3xl dark:text-white">Fotos</h1>
+      
+      <h1 class="text-center font-bold py-10 text-3xl dark:text-white">Fotos</h1>
 
-      <div className="lg:px-32 p-4 items-center">
-        <div className="grid grid-cols-4 grid-rows-4 grid-flow-col gap-2">
-          <a href="#gallery-5" className="w-full row-span-2">
-            <img src={Photo06} alt="Casamento do meu irmão." className="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer" />
-          </a>
-          <a href="#gallery-1" className="w-full col-span-2 row-span-2">
-            <img src={Photo02_full} alt="Photo by Claudio Schwarz on Unsplash" className="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer" />
-          </a>
-          <a href="#gallery-3" className="w-full">
-            <img src={Photo04} alt="Photo by Claudio Schwarz on Unsplash" className="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer" />
-          </a>
-          <a href="#gallery-4" className="w-full">
-            <img src={Photo05} alt="Photo by Claudio Schwarz on Unsplash" className="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer" />
-          </a>
-          <a href="#gallery-2" className="w-full col-span-2 row-span-2">
-            <img src={Photo03} alt="Photo by Claudio Schwarz on Unsplash" className="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer" />
-          </a>
-          <a href="#gallery-6" className="w-full">
-            <img src={Photo11} alt="Photo by Claudio Schwarz on Unsplash" className="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer" />
-          </a>
-          <a href="#gallery-8" className="w-full">
-            <img src={Photo09} alt="Photo by Claudio Schwarz on Unsplash" className="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer" />
-          </a>
-          <a href="#gallery-7" className="w-full">
-            <img src={Photo07} alt="Photo by Claudio Schwarz on Unsplash" className="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer" />
-          </a>
-          <a href="#gallery-9" className="w-full">
-            <img src={Photo08} alt="Photo by Claudio Schwarz on Unsplash" className="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer" />
-          </a>
+      <div class="container lg:px-32 px-4 py-8 mx-auto items-center ">
+        <div class="grid grid-cols-4 grid-rows-4 grid-flow-col gap-2">
+          <div class="w-full row-span-2">
+            <img src={Photo06} alt="Casamento do meu irmão." class="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer"/>
+          </div>
+          <div class="w-full col-span-2 row-span-2">
+            <img
+              src={Photo02_full}
+              alt="Photo by Claudio Schwarz on Unsplash"
+              class="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer"/>
+          </div>
+          <div class="w-full ">
+            <img
+              src={Photo04}
+              alt="Photo by Claudio Schwarz on Unsplash"
+              class="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer"/>
+          </div>
+          <div class="w-full">
+            <img
+              src={Photo05}
+              alt="Photo by Claudio Schwarz on Unsplash"
+              class="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer"/>
+          </div>
+          <div class="w-full col-span-2 row-span-2">
+            <img
+              src={Photo03}
+              alt="Photo by Claudio Schwarz on Unsplash"
+              class="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer"/>
+          </div>
+
+          <div class="w-full">
+            <img
+              src={Photo11}
+              alt="Photo by Claudio Schwarz on Unsplash"
+              class="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer"/>
+          </div>
+          <div class="w-full">
+            <img
+              src={Photo09}
+              alt="Photo by Claudio Schwarz on Unsplash"
+              class="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer"/>
+          </div>
+          <div class="w-full">
+            <img
+              src={Photo07}
+              alt="Photo by Claudio Schwarz on Unsplash"
+              class="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer"/>
+          </div>
+          <div class="w-full">
+            <img
+              src={Photo08}
+              alt="Photo by Claudio Schwarz on Unsplash"
+              class="inset-0 h-full w-full object-cover object-center rounded hover:scale-110 cursor-pointer"/>
+          </div>
         </div>
       </div>
 
-      <Modal/>
-
-      {/* <div id="modal" className="hidden fixed p-5 top-0 left-0 z-80 w-screen h-screen bg-black/70 flex justify-center items-center">
-        <a className="fixed top-2 right-2 text-white btn text-5xl" href="javascript:void(0)" onClick={() => { closeModal() }}>&times;</a>
-
-        {
-          image == 1 ? <img id="modal-img" src={Photo06} alt="Casamento do meu irmão." className="max-w-[200px] max-h-[600px] object-cover rounded-lg" /> : undefined
-        }
-
-        {
-          image == 2 ? <img id="modal-img" src={Photo02_full} alt="Casamento do meu irmão." className="max-w-[800px] max-h-[600px] object-cover rounded-lg" /> : undefined
-        }
-
-        {
-          image == 3 ? <img id="modal-img" src={Photo04} alt="Casamento do meu irmão." className="max-w-[800px] max-h-[600px] object-cover rounded-lg" /> : undefined
-        }
-
-        {
-          image == 4 ? <img id="modal-img" src={Photo05} alt="Casamento do meu irmão." className="max-w-[800px] max-h-[600px] object-cover rounded-lg" /> : undefined
-        }
-
-        {
-          image == 5 ? <img id="modal-img" src={Photo03} alt="Casamento do meu irmão." className="max-w-[800px] max-h-[600px] object-cover rounded-lg" /> : undefined
-        }
-
-        {
-          image == 6 ? <img id="modal-img" src={Photo11} alt="Casamento do meu irmão." className="max-w-[800px] max-h-[600px] object-cover rounded-lg" /> : undefined
-        }
-
-        {
-          image == 7 ? <img id="modal-img" src={Photo09} alt="Casamento do meu irmão." className="max-w-[800px] max-h-[600px] object-cover rounded-lg" /> : undefined
-        }
-
-        {
-          image == 8 ? <img id="modal-img" src={Photo07} alt="Casamento do meu irmão." className="max-w-[800px] max-h-[600px] object-cover rounded-lg" /> : undefined
-        }
-
-        {
-          image == 9 ? <img id="modal-img" src={Photo08} alt="Casamento do meu irmão." className="max-w-[800px] max-h-[600px] object-cover rounded-lg" /> : undefined
-        }
-
-      </div> */}
     </div>
   );
 }

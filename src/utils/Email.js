b787a038ceae1608e1ca9fba1a6024a90faa2503
Julio Thomazelli <juliothomazelli@gmail.com>
2023-08-19 16:38:08 -0300

    Update Email.js

--- src/utils/Email.js
+++ src/utils/Email.js
@@ -1,8 +1,9 @@
-import axios from "axios";
+import axios, {AxiosRequestConfig} from "axios";
 
 export default class Email {
   
   async sendMessage(data){
-    await axios({url: "http://149.100.142.228:3000/email/send", data: data, method: "POST"});  
+    // await axios({url: "http://localhost:3000/email/send", data: data, method: "POST"});
+    await axios("http://149.100.142.228:3000/email/send", { method: "POST", data: data });
   }
 }

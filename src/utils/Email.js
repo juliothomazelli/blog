4bdee3917ba42fee872c02a6ea4b79116c5518b8
Julio Thomazelli <juliothomazelli@gmail.com>
2023-08-19 16:54:16 -0300

    Update Email.js

--- src/utils/Email.js
+++ src/utils/Email.js
@@ -2,7 +2,30 @@ import axios, {AxiosRequestConfig} from "axios";
 
 export default class Email {
   
-  async sendMessage(data){
-    await axios({url: "http://149.100.142.228:3000/email/send", data: data, method: "POST"});
+  async sendMessage(asdas){
+    let data = JSON.stringify({
+      "name": "Maria Gabriela",
+      "email": "teste@testando.com",
+      "message": "Estou mandando essa mensagem para informar que estou entrando em contato"
+    });
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'http://149.100.142.228:3000/email/send',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+
+    axios.request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   }
 }
